34352275fdf79a7f33e587358b8a8bd7b45e3d49
LDbird <1337828916@qq.com>
2019-08-18 22:10:13 +0800

    头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,22 @@
 <template>
   <el-container class="container">
-    <el-header class="header">Header</el-header>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+            <img src="../../images/tx2.jpg" alt="">
+          </div>
+        </el-col>
+        <el-col :span="18" class="middle">
+          <h3>电商后台管理系统</h3>
+        </el-col>
+        <el-col :span="2">
+          <div class="grid-content bg-purple">
+            <a class="logout" href="">退出</a>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside width="200px" class="aside">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -37,5 +53,17 @@
     color: #333;
     text-align: center;
     line-height: 160px;
+  }
+
+  .header .grid-content img{
+    width: 60px;
+    height: 60px;
+    border-radius: 100%;
+  }
+  .middle{
+    line-height: 60px;
+  }
+  .logout{
+    text-decoration: none;
   }
 </style>
